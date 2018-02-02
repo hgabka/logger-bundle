cc2cda765241b705410ed628729b016a7d9da83b
Gabe <hgabka@dev.hgabka.eu>
2018-02-02 12:48:01 +0100

    loggable controller trait

--- src/Controller/LoggableController.php
+++ src/Controller/LoggableController.php
@@ -2,55 +2,10 @@
 
 namespace Hgabka\LoggerBundle\Controller;
 
-use Hgabka\LoggerBundle\Event\LogActionEvent;
-use Monolog\Logger;
+use Hgabka\LoggerBundle\Traits\LoggableTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LoggableController extends Controller
 {
-    protected function logStart($type, $params = [], $priority = null)
-    {
-        $priority = $priority ?? Logger::getLevelName(Logger::INFO);
-
-        $event = new LogActionEvent();
-        $event
-            ->setType($type)
-            ->setParameters($params)
-            ->setPriority($priority)
-        ;
-
-        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_START, $event);
-    }
-
-    protected function logDone()
-    {
-        $event = new LogActionEvent();
-
-        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_DONE, $event);
-    }
-
-    protected function logUpdate()
-    {
-        $event = new LogActionEvent();
-
-        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_UPDATE, $event);
-    }
-
-    protected function logError()
-    {
-        $event = new LogActionEvent();
-        $event
-            ->setParameters(null)
-            ->setPriority(Logger::getLevelName(Logger::ERROR))
-        ;
-
-        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_UPDATE, $event);
-        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_DONE, $event);
-    }
-
-    protected function actionLog($type, $params = [], $priority = null)
-    {
-        $this->logStart($type, $params, $priority);
-        $this->logDone();
-    }
+    use LoggableTrait;
 }
---
+++ src/Traits/LoggableTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Hgabka\LoggerBundle\Traits;
+
+use Hgabka\LoggerBundle\Event\LogActionEvent;
+use Monolog\Logger;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+trait LoggableTrait
+{
+    protected function logStart($type, $params = [], $priority = null)
+    {
+        $priority = $priority ?? Logger::getLevelName(Logger::INFO);
+
+        $event = new LogActionEvent();
+        $event
+            ->setType($type)
+            ->setParameters($params)
+            ->setPriority($priority)
+        ;
+
+        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_START, $event);
+    }
+
+    protected function logDone()
+    {
+        $event = new LogActionEvent();
+
+        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_DONE, $event);
+    }
+
+    protected function logUpdate()
+    {
+        $event = new LogActionEvent();
+
+        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_UPDATE, $event);
+    }
+
+    protected function logError()
+    {
+        $event = new LogActionEvent();
+        $event
+            ->setParameters(null)
+            ->setPriority(Logger::getLevelName(Logger::ERROR))
+        ;
+
+        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_UPDATE, $event);
+        $this->get('event_dispatcher')->dispatch(LogActionEvent::EVENT_DONE, $event);
+    }
+
+    protected function actionLog($type, $params = [], $priority = null)
+    {
+        $this->logStart($type, $params, $priority);
+        $this->logDone();
+    }
+}
