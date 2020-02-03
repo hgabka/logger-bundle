9d09f0bd2ffd1a0d1a8c2a0104ae17c7cd18c47d
hgabka <hgabka@gmail.com>
2020-02-03 23:59:59 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -26,8 +26,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hgabka_logger');
+        $treeBuilder = new TreeBuilder('hgabka_logger');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
