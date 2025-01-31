259d127bb935d0acbffc0353b0f160a20bde5ad1
hgabka <hgabka@gmail.com>
2025-01-31 19:48:05 +0100

    Update HgabkaLoggerExtension.php

--- src/DependencyInjection/HgabkaLoggerExtension.php
+++ src/DependencyInjection/HgabkaLoggerExtension.php
@@ -5,8 +5,9 @@ namespace Hgabka\LoggerBundle\DependencyInjection;
 use Hgabka\LoggerBundle\Helper\ExceptionNotifier;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
 
 /**
  * This is the class that loads and manages your bundle configuration.
